b199a50deaf0409d25f16bb13620673a6e19d7db
markgtomas <mark.tomas@ucdconnect.ie>
2025-10-08 15:13:24 +0100

    who likes code?

--- Main code (maybe)/main.cpp
+++ Main code (maybe)/main.cpp
@@ -1,13 +1,35 @@
 //include libraries
-#include <servo.h>
+#include <Servo.h>
 
-Servo leg[8][3] = {
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {}
+Servo servo[8][3];
+
+// placeholder pins
+int PIN[8][3] = { // get someone to figure out the pin numbers for the I^2c extention
+  // leg0: front-left
+  {0, 1, 2}, 
+  // leg1: front-right
+  {3, 4, 5},
+  // leg2: middle-front-left
+  {6, 7, 8},
+  // leg3: middle-front-right
+  {9, 10, 11},
+  // leg4: middle-bottom-left
+  {12, 13, 14},
+  // leg5: middle-bottom-right
+  {15, 16, 17},
+  // leg6: bottom-left
+  {18, 19, 20},
+  // leg7: bottom-right
+  {21, 22, 23}
+};
+
+void setup(){
+  for (int leg = 0; leg < 8; leg++){
+    for (int limb = 0; limb < 3; limb++){
+      servo[leg][limb].attach(PIN[leg][limb]);
+    }
+  }
+}
+
+void loop(){
 }
